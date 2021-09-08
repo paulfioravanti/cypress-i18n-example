2cbc1624a156fd0c7e54d5c064ac54ca998ac885
Paul Fioravanti <paul.fioravanti@gmail.com>
2021-09-08 22:04:23 +1000

    Add { force: true } to dropdown menu click options otherwise it would seem the clicks do not always register with the Phoenix application :(

--- cypress/support/commands/language_menu.js
+++ cypress/support/commands/language_menu.js
@@ -21,5 +21,11 @@ Cypress.Commands.add("clickJapanese", () => {
 })
 
 function clickMenuLabel(label) {
-  cy.get("li").contains(label).click()
+  // NOTE: { force: true } added for the Phoenix application's sake. There
+  // would seem to be times during the "changing language" and "storing language
+  // setting" tests where the click to open the menu does not register or just
+  // gets lost, leading to the CSS of the dropdown menu still being set to
+  // display: none :(
+  // It would be great to be able to remove { force: true } in the future...
+  cy.get("li").contains(label).click({ force: true })
 }

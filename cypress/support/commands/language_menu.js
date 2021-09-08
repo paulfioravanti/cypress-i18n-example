876404aeb485522de7c64cb95c33c2c9ffa443b8
Paul Fioravanti <paul.fioravanti@gmail.com>
2021-09-08 20:50:13 +1000

    Update Cypress

--- cypress/support/commands/language_menu.js
+++ cypress/support/commands/language_menu.js
@@ -4,6 +4,10 @@ import {
   JAPANESE_MENU_LABEL,
 } from "./constants"
 
+Cypress.Commands.add("clickLanguageMenu", () => {
+  cy.get("p").click()
+})
+
 Cypress.Commands.add("clickEnglish", () => {
   clickMenuLabel(ENGLISH_MENU_LABEL)
 })
@@ -14,10 +18,6 @@ Cypress.Commands.add("clickItalian", () => {
 
 Cypress.Commands.add("clickJapanese", () => {
   clickMenuLabel(JAPANESE_MENU_LABEL)
-})
-
-Cypress.Commands.add("clickLanguageMenu", () => {
-  cy.get("p").click()
 })
 
 function clickMenuLabel(label) {
